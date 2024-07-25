655769ec2006239403a6a49df9baa67e9487372f
jshlomov <jonatanshv@gmail.com>
2024-07-25 17:25:38 +0300

    Add project files.

---
+++ CarMvc/Controllers/Car.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarMvc.Controllers
+{
+    public class Car : Controller
+    {
+
+
+        public IActionResult Model(string year = "2015", string name = "mazda", string color = "red")
+        {
+            var car = new Models.Car()
+            {
+                Year = year,
+                Name = name,
+                Color = color
+            };
+            return View(car);
+        }
+    }
+}
---
+++ CarMvc/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace CarMvc.Models
+{
+    public class Car
+    {
+        public string Year { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}
